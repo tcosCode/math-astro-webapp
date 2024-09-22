46b3f0e2beb027f5be480189cef7a201a769e022
Yordis Monteserin <tcoscode@gmail.com>
2024-09-22 02:06:55 -0700

    exercises page added

--- src/data/topics.js
+++ src/data/topics.js
@@ -1,15 +1,32 @@
+const TRULYFALSY = "TrulyFalsy";
+const CHOOSEANSWER = "ChooseAnswer";
+
 export const topics = [
   {
     title: "7mo",
     img: "/img/calculator.svg",
     list: ["Álgebra", "Geometría", "Números Racionales"],
-    exercises: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+    exercises: [
+      {
+        id: "1",
+        type: CHOOSEANSWER,
+        title: "Componer",
+        texto: "Por añadir:",
+      },
+    ],
   },
   {
     title: "8vo",
     img: "/img/online-class-27.svg",
     list: ["Funciones", "Estadística", "Probabilidad"],
-    exercises: ["1", "2", "3", "4", "5", "6", "7", "8"],
+    exercises: [
+      {
+        id: "1",
+        type: CHOOSEANSWER,
+        title: "Componer",
+        texto: "Por añadir:",
+      },
+    ],
   },
   {
     title: "9no",
@@ -18,37 +35,57 @@ export const topics = [
     exercises: [
       {
         id: "1",
+        type: CHOOSEANSWER,
         title: "Descomponer en factores",
         texto:
           "Descomponer en factores. Trabaje en su libreta y luego seleccione una opción como resultado:",
       },
       {
         id: "2",
+        type: CHOOSEANSWER,
         title: "Factorizar",
         texto:
           "Factorizar completamente. Trabaje en su libreta y luego seleccione una opción como resultado:",
       },
-      "3",
-      "4",
-      "5",
     ],
   },
   {
     title: "10mo",
     img: "/img/video-tutorial.svg",
     list: ["Álgebra Avanzada", "Geometría Euclidiana", "Funciones y Gráficas"],
-    exercises: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    exercises: [
+      {
+        id: "1",
+        type: CHOOSEANSWER,
+        title: "Componer",
+        texto: "Por añadir:",
+      },
+    ],
   },
   {
     title: "11no",
     img: "/img/coach-85.svg",
     list: ["Cálculo Diferencial", "Estadística", "Geometría Analítica"],
-    exercises: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+    exercises: [
+      {
+        id: "1",
+        type: CHOOSEANSWER,
+        title: "Componer",
+        texto: "Por añadir:",
+      },
+    ],
   },
   {
     title: "12vo",
     img: "/img/presentation.svg",
     list: ["Cálculo Integral", "Álgebra Lineal", "Mat. Discretas"],
-    exercises: ["1", "2", "3", "4", "5", "6", "7"],
+    exercises: [
+      {
+        id: "1",
+        type: CHOOSEANSWER,
+        title: "Componer",
+        texto: "Por añadir:",
+      },
+    ],
   },
 ];
