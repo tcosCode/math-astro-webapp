07683d81964386103abe43c9b7127b4f5ac17362
Yordis Monteserin <tcoscode@gmail.com>
2024-09-23 02:04:00 -0700

    choose page finish

--- src/data/topics.js
+++ src/data/topics.js
@@ -1,6 +1,9 @@
 const TRULYFALSY = "TrulyFalsy";
 const CHOOSEANSWER = "ChooseAnswer";
 
+const CORRECT = "correct";
+const INCORRECT = "incorrect";
+
 export const topics = [
   {
     title: "7mo",
@@ -9,7 +12,7 @@ export const topics = [
     exercises: [
       {
         id: "1",
-        type: CHOOSEANSWER,
+        type: "",
         title: "Componer",
         texto: "Por añadir:",
       },
@@ -17,12 +20,12 @@ export const topics = [
   },
   {
     title: "8vo",
-    img: "/img/online-class-27.svg",
+    img: "/img/book-lover.svg",
     list: ["Funciones", "Estadística", "Probabilidad"],
     exercises: [
       {
         id: "1",
-        type: CHOOSEANSWER,
+        type: "",
         title: "Componer",
         texto: "Por añadir:",
       },
@@ -39,6 +42,89 @@ export const topics = [
         title: "Descomponer en factores",
         texto:
           "Descomponer en factores. Trabaje en su libreta y luego seleccione una opción como resultado:",
+        incisos: [
+          {
+            id: "a",
+            exercise: "8a - 8b",
+            answer: [
+              { text: "(a - 8)(b - 8)", validation: INCORRECT },
+              { text: "8(a - 8b)", validation: INCORRECT },
+              { text: "8(a - b)", validation: CORRECT },
+            ],
+          },
+          {
+            id: "b",
+            exercise: "x<sup>2</sup> - 2x - 48",
+            answer: [
+              { text: "(x - 8)(x - 6)", validation: INCORRECT },
+              { text: "(x - 8)(x + 6)", validation: CORRECT },
+              { text: "D &lt; 0", validation: INCORRECT },
+            ],
+          },
+          {
+            id: "c",
+            exercise: "y<sup>2</sup> - 49",
+            answer: [
+              { text: "(y + 7)(y - 7)", validation: INCORRECT },
+              { text: "(y + 7)(y + 7y)", validation: INCORRECT },
+              { text: "(y - 7)(y + 7)", validation: CORRECT },
+            ],
+          },
+          {
+            id: "d",
+            exercise: "2m<sup>2</sup> - 17m - 48",
+            answer: [
+              { text: "(2m + 1)(m + 8)", validation: INCORRECT },
+              { text: "(2m - 1)(m - 8)", validation: CORRECT },
+              { text: "(2m + 1)(m - 8)", validation: INCORRECT },
+            ],
+          },
+          {
+            id: "e",
+            exercise: "y<sup>2</sup> - 7",
+            answer: [
+              {
+                text: "(y - 7<sup>1/2</sup>)(y - 7<sup>1/2</sup>)",
+                validation: INCORRECT,
+              },
+              {
+                text: "(y + 7<sup>1/2</sup>)(y + 7<sup>1/2</sup>)",
+                validation: INCORRECT,
+              },
+              {
+                text: "(y + 7<sup>1/2</sup>)(y - 7<sup>1/2</sup>)",
+                validation: CORRECT,
+              },
+            ],
+          },
+          {
+            id: "f",
+            exercise: "5n<sup>2</sup> + 12n + 4",
+            answer: [
+              { text: "(5n + 2)(n - 2)", validation: INCORRECT },
+              { text: "(5n + 2)(n + 2)", validation: CORRECT },
+              { text: "(5n - 2)(n + 2)", validation: INCORRECT },
+            ],
+          },
+          {
+            id: "g",
+            exercise: "x<sup>2</sup> - x + 8",
+            answer: [
+              { text: "D &lt; 0", validation: CORRECT },
+              { text: "(x + 2)(x + 4)", validation: INCORRECT },
+              { text: "(x - 2)(x + 4)", validation: INCORRECT },
+            ],
+          },
+          {
+            id: "h",
+            exercise: "v<sup>2</sup> - 36 + 12v",
+            answer: [
+              { text: "(v - 14.4)(v - 2.4)", validation: CORRECT },
+              { text: "(v + 14.4)(v + 2.4)", validation: INCORRECT },
+              { text: "(v + 14.4)(v - 2.4)", validation: INCORRECT },
+            ],
+          },
+        ],
       },
       {
         id: "2",
@@ -69,7 +155,7 @@ export const topics = [
     exercises: [
       {
         id: "1",
-        type: CHOOSEANSWER,
+        type: TRULYFALSY,
         title: "Componer",
         texto: "Por añadir:",
       },
@@ -82,7 +168,7 @@ export const topics = [
     exercises: [
       {
         id: "1",
-        type: CHOOSEANSWER,
+        type: TRULYFALSY,
         title: "Componer",
         texto: "Por añadir:",
       },
