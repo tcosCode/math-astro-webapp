d34596195a242e3c3e3017407791f5f67a86c97b
Yordis Monteserin <tcoscode@gmail.com>
2024-11-10 14:49:09 -0800

    added ex 11no

--- src/data/grades/11no.js
+++ src/data/grades/11no.js
@@ -16,9 +16,49 @@ export const grades11no = {
   exercises: [
     {
       id: "1",
-      type: TRUEFALSE,
-      title: "Componer",
-      texto: "Por añadir:",
+      type: "ChooseAnswer",
+      title: "Distancia entre puntos ",
+      texto: "Calcular luego seleccionar la respuesta correcta.",
+      incisos: [
+        {
+          id: "a",
+          exercise:
+            "A(-2; 1)&nbsp;&nbsp;y&nbsp;&nbsp;B(4;<sup>1</sup>/<sub>3</sub>",
+          answer: [
+            {
+              text: "5,93",
+              validation: "incorrect",
+            },
+            {
+              text: "6,03",
+              validation: "correct",
+            },
+            {
+              text: "7,12",
+              validation: "incorrect",
+            },
+          ],
+        },
+        {
+          id: "b",
+          exercise:
+            "A(2<sup>3</sup>; 1)&nbsp;&nbsp;y&nbsp;&nbsp;B(4; <sup>1</sup>/<sub>2</sub>)",
+          answer: [
+            {
+              text: "2,18",
+              validation: "incorrect",
+            },
+            {
+              text: "3,21",
+              validation: "incorrect",
+            },
+            {
+              text: "4,03",
+              validation: "correct",
+            },
+          ],
+        },
+      ],
     },
   ],
 };
