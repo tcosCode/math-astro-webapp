3c2312e322a36beec0f080b9c2b4e74bcb1db686
Yordis Monteserin <tcoscode@gmail.com>
2025-01-25 13:40:20 -0800

    added styles for classes page

--- src/data/constants.js
+++ src/data/constants.js
@@ -3,104 +3,125 @@
 export const classes = [
   {
     title: "Ecuaciones exponenciales",
+    grade: "11no",
     image: "XdaB464Gt4M", //Youtube video Id to capture the thumbnail
     url: "PLeySRPnY35dHJJIKyqY0Wg4TTM0NDsS7f",
   },
   {
     title: "Funciones compuestas",
+    grade: "11no",
     image: "RIgzDV-AV_Q",
     url: "PLeySRPnY35dEKKcGIdu6NYgMShNyuK9rA",
   },
   {
     title: "Ecuaciones trigonométricas",
+    grade: "10mo",
     image: "ZTRETZaSx0s",
     url: "PLeySRPnY35dEmHXbIXk9NjxWA_XzxkyEI",
   },
   {
     title: "Sistema de ecuaciones lineales 2x2",
+    grade: "7mo",
     image: "oQQfG1zIPMc",
     url: "PLeySRPnY35dErygDdRDp1912SPALoaBmZ",
   },
   {
     title: "Logaritmos",
+    grade: "10mo, 11no",
     image: "pZTuEHrnOMg",
     url: "PLeySRPnY35dHyUzy-YVDD9ZllhtXfcQ4_",
   },
   {
     title: "Ecuación de la recta",
+    grade: "11no",
     image: "GBSmycLgTeU",
     url: "PLeySRPnY35dE1JAjLtnjoDTA5-oWq6m2w",
   },
   {
     title: "Estadística",
+    grade: "8vo, 9no",
     image: "CWDYwZ_c1lI",
     url: "PLeySRPnY35dFF5D9g_zi07yPKGXui4GII",
   },
   {
     title: "División de polinomios",
+    grade: "8vo",
     image: "PxycywivGUQ",
     url: "PLYELZSZcFU6UVjDuVY2xxaFk1jzTAKpYm",
   },
   {
     title: "Ecuaciones cuadráticas",
+    grade: "9no",
     image: "aYkOsFHCqPM",
     url: "PLeySRPnY35dF11EWceCxzKtcaZidCkXXh",
   },
   {
     title: "Razones trigonométricas",
+    grade: "11no",
     image: "7pUi5lvLf7c",
     url: "PLeySRPnY35dEAIFYvOhtD2cztVuq15qw1",
   },
   {
     title: "Funciones",
+    grade: "10mo, 12mo",
     image: "HAeSkQH1C-I",
     url: "PLeySRPnY35dGfEuNGbQmymhiQF4oTUIMb",
   },
   {
     title: "Funciones cuadráticas",
+    grade: "10mo",
     image: "YlhOfpREfHE", // if !playlist, then the video will be captured from the thumbnail
   },
   {
     title: "Funciones lineales",
+    grade: "10mo",
     image: "AoZpzAoC1Qg",
   },
   {
     title: "Números complejos",
+    grade: "12mo",
     image: "KzMSuHApEvk",
     url: "PLeySRPnY35dHfzYRb8StWkxnVTkrocv6X",
   },
   {
     title: "Funciones inversas",
+    grade: "12mo",
     image: "RIgzDV-AV_Q",
     url: "PLeySRPnY35dEKKcGIdu6NYgMShNyuK9rA",
   },
   {
     title: "Funciones compuestas",
+    grade: "12mo",
     image: "RIgzDV-AV_Q",
     url: "PLeySRPnY35dEKKcGIdu6NYgMShNyuK9rA",
   },
   {
     title: "Factorización",
+    grade: "9no",
     image: "sSfO1CsKJ4g",
     url: "PLeySRPnY35dGY6GX7xO_lruvCIS6NkfR-",
   },
   {
     title: "Área y Perímetro",
+    grade: "9no",
     image: "DPcP6gz495U",
     url: "PLeySRPnY35dGL6Gpm_T-30RH3Kr2IxeDN",
   },
 
   {
     title: "Logaritmos",
+    grade: "10mo, 11no",
     image: "pZTuEHrnOMg",
     url: "PLeySRPnY35dHyUzy-YVDD9ZllhtXfcQ4",
   },
   {
     title: "Igualdad de triángulos",
+    grade: "8vo",
     image: "hhpN91CSJpY",
   },
   {
     title: "Identidades Trigonométricas",
+    grade: "11no",
     image: "PbvKVSWyvpI",
     url: "PLeySRPnY35dHK3mo8UWd3zAnYCG13OgAR",
   },
