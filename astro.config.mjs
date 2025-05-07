95f96199556717cb8a698f06374c247dc143b589
Yordis Monteserin <tcoscode@gmail.com>
2025-05-07 10:32:07 -0700

    update astro, fixed transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import clerk from "@clerk/astro";
+import { esES } from "@clerk/localizations";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [clerk()],
+  integrations: [clerk({ localization: esES })],
   adapter: node({ mode: "standalone" }),
   output: "server",
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
-const isProtectedRoute = createRouteMatcher(["/7mo(*)", "/8vo(*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/7mo(.*)",
+  "/8vo(.*)",
+  "/9no(.*)",
+  "/10mo(.*)",
+  "/11no(.*)",
+  "/12mo(.*)",
+]);
 
 export const onRequest = clerkMiddleware((auth, context) => {
   const { userId, redirectToSignIn } = auth();
