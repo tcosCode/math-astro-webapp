67fe47f7e155136bcccd7258c5fb746ae8d77ded
Yordis Monteserin <tcoscode@gmail.com>
2025-05-07 19:55:09 -0700

    fixed astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+//import node from "@astrojs/node";
 import clerk from "@clerk/astro";
 import { esES } from "@clerk/localizations";
 import netlify from "@astrojs/netlify/functions";
@@ -8,9 +8,7 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   integrations: [clerk({ localization: esES })],
-  adapter: netlify({
-    edgeMiddleware: true,
-  }),
+  adapter: netlify({}),
   //adapter: node({ mode: "standalone" }),
   output: "server",
 });
